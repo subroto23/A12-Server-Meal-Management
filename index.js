870d44cc54da70731c6d422800f19d44f883a2ad
Subroto Das <rupashi.bangla.club@gmail.com>
2023-12-04 08:18:16 +0600

    Some CHanges

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// const app = require("./src/App");
-const express = require("express");
-const app = express();
-// const { mongodbConnection } = require("./src/Config/MongodbConfig");
-// const { PortNumber } = require("./src/Secret");
-
-const PortNumber = process.env.PORT || 5001;
+const app = require("./src/App");
+const { mongodbConnection } = require("./src/Config/MongodbConfig");
+const { PortNumber } = require("./src/Secret");
 
 app.get("/", (req, res) => {
   res.send("A12 Server is Running");
@@ -14,5 +10,5 @@ app.get("/", (req, res) => {
 app.listen(PortNumber, async () => {
   console.log(`Server is Running at http://localhost:${PortNumber}`);
   //Mongodb Connections
-  // await mongodbConnection();
+  await mongodbConnection();
 });
