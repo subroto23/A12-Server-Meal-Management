c42df97efbf7c8f230e5a4cb7b10a5338c8c7cf3
Subroto Das <rupashi.bangla.club@gmail.com>
2023-12-04 07:28:09 +0600

    Server HomePage

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require("./src/App");
 const { mongodbConnection } = require("./src/Config/MongodbConfig");
 const { PORT } = require("./src/Secret");
 
+app.get("/", (req, res) => {
+  res.send("A12 Server Running");
+});
+
 //Server Preview Check
 app.listen(PORT, async () => {
   console.log(`Server is Running at http://localhost:${PORT}`);
