fa5abdcb02edaf1824623ac81c0f9270cee452be
Subroto Das <rupashi.bangla.club@gmail.com>
2024-01-12 08:31:32 +0600

    New Services adding

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const OrderRouter = require("./Router/OrderRoute/OrderRouter");
 //middleWare
 app.use(
   cors({
-    origin: ["https://hostel-management-delta.vercel.app"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
--- src/Controller/PaymentController/PaymentPostController.js
+++ src/Controller/PaymentController/PaymentPostController.js
@@ -1,16 +1,16 @@
 const { payment_secret } = require("../../Secret");
-const stripe = require("stripe")(payment_secret);
+const stripe = require("stripe")(`${payment_secret}`);
 
 const PaymentPostController = async (req, res, next) => {
   try {
     const { price } = req.body;
     const amount = parseInt(Number(price) * 100);
-    const paymentIntent = await stripe.paymentIntents.create({
+    const payment = await stripe.paymentIntents.create({
       amount: amount,
       currency: "usd",
       payment_method_types: ["card"],
     });
-    res.send({ clientSecret: paymentIntent.client_secret });
+    res.send({ clientSecret: payment. client_secret });
   } catch (error) {
     console.log(error);
   }
